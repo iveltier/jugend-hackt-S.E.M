c816aa27fa6de8422f9fd3bb65c33e2f2aa66eb2
iveltier <iveltier@mailbox.org>
2025-04-13 11:04:05 +0200

    Verschlüsslung.py

--- server_auto.py
+++ server_auto.py
@@ -1,6 +1,67 @@
 import socket
 import display
 
+def coden(Antwort):
+
+    Naricht = "".join(Antwort.split()) #list( Antwort.split())  #Geben Sie die Naricht ein. Trenne Buchstaben und Zeichen durch leerzeichen
+    Alphabet = []
+    x = 97
+    while x < 123:
+        Buchstaben = chr(x)
+        Alphabet.append(Buchstaben)
+        x=x+1
+    code = []
+    #print (Alphabet)
+    x = 0
+    output = ''
+    Naricht_länge = len(Naricht)
+    while x < Naricht_länge:
+        Zeichen = Naricht[x]
+        #print ("zeichen=", Zeichen)
+        Zeichen_Index = Alphabet.index(Zeichen)
+        #print (Zeichen_Index)
+        neues_Zeichen = Alphabet[(Zeichen_Index + 4)%26]
+        #print (neues_Zeichen)
+        Zahl = ord(neues_Zeichen)
+        #print (Zahl)
+        neue_Zahl = Zahl * 2
+        #print (neue_Zahl," ",end="")
+        output = output + str (neue_Zahl) +" "
+        x = x+1
+    return output
+#result = coden(input())
+
+
+def entcoden(Antwort):
+    Naricht = list(map(int, Antwort.split())) #Geben Sie die Naricht ein. Trenne Buchstaben und Zeichen durch leerzeichen
+    Alphabet = []
+    x = 97
+    while x < 123:
+        Buchstaben = chr(x)
+        Alphabet.append(Buchstaben)
+        x=x+1
+    code = []
+    #print (Alphabet)
+    x = 0
+    Naricht_länge = len(Naricht)
+    output = ""
+    while x < Naricht_länge:
+        Zahl = Naricht[x]
+        #print (Zahl)
+        neue_Zahl = Zahl / 2
+        neue_Zahl = int(neue_Zahl)
+        neues_Zeichen = chr(neue_Zahl)
+        Zeichen_Index = Alphabet.index(neues_Zeichen)
+        if Zeichen_Index < 4 :
+            Zeichen = Alphabet[(Zeichen_Index - 4+26)%26]
+        else:
+            Zeichen = Alphabet[(Zeichen_Index - 4)%26]
+        #print (Zeichen, end="")
+        output = output + Zeichen
+        x = x+1
+    return output
+# result_empfaenger = entcoden(input())
+
 def get_local_ip():
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -22,10 +83,11 @@ def start_server():
     display.set_text_upper(f"Client {addr[0]} verbunden!")
     
     while True:
-        data = conn.recv(1024).decode()
+        data = conn.recv(1024)
         if not data: break
+        data = entcoden(data)
         display.set_text_upper(f"{data}")
-        conn.sendall(input("Antwort: ").encode())
+        conn.sendall(coden(input("Antwort: ")))
 
 if __name__ == "__main__":
     start_server()
